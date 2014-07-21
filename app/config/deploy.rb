75e3fbeefe3f79a366da2a3716dd1655a1489945
ahaifi <ahaifi@annoncesjaunes.com>
2014-07-21 13:21:05 +0200

    tes

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,28 +1,50 @@
-set :application, "kapstages"
-set :domain,      "192.168.56.4"
-set :user,     "vagrant"
-default_run_options[:pty] = true
-set :use_sudo,  true
-set :deploy_to,   "/vagrant"
-set :app_path,    "app"
+set :application, "Nom de votre projet"
+set :domain, "localhost" # Le SSH de destination
+set :deploy_to, "/home/ahaifi/data/test" # Le répertoire de destination
+set :app_path, "app" # Le dossier d’application, laissez app
+set :user, "ahaifi" # Le nom d’utilisateur du serveur distant
+set :password, "123456789" # Le nom d’utilisateur du serveur distant
 
-set :repository,  "git@github.com:anasstsdi/kapstage.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :copy_dir, "/home/ahaifi/tmp"
+set :remote_copy_dir, "/tmp"
 
-set :model_manager, "doctrine"
-# Or: `propel`
+set :repository, "git@github.com:anasstsdi/gripli.git" # L’URL de votre repository
+set :branch, "master" # La branche Git, utile si vous pushez vos releases de prod sur une branche particulière
+set :scm, :git # SVN ? Haha, vous plaisantez j’espère :-)
+set :deploy_via, :copy # Ils y a plusieurs méthodes de déploiements, nous utilisons la méthode de copy
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set :shared_files,     ["app/config/parameters.yml"]
-set :shared_children,  [app_path + "/logs", web_path + "/uploads"]
 
-set :use_composer,   true
-set :update_vendors, true
+role :web, domain
+role :app, domain
+set :php_bin,      "php"
+# Nous utilisons sudo pour régler les permissions via la methode :chown
+# préférez l’utilisation des ACLs si c’est disponible sur votre serveur
 
-set  :keep_releases,  3
+set :use_sudo, false
+set :keep_releases, 3 # Le nombre de releases à garder après un déploiement réussi
 
-# Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+## Symfony2
+set :shared_files, ["app/config/parameters.yml"] # Les fichiers à conserver entre chaque déploiement
+set :shared_children, [app_path + "/logs", "vendor"] # Idem, mais pour les dossiers
+set :use_composer, true
+set :update_vendors, false # Il est conseillé de laisser a false et de ne pas faire de ‘composer update’ directement sur la prod
+#set :composer_options, "--verbose --prefer-dist" # Permet de spécifier des paramètres supplémentaires à composer, inutile dans notre cas
+set :writable_dirs, ["app/cache", "app/logs"] # Application des droits nécessaires en écriture sur les dossiers
+set :webserver_user, "www-data" # L’utilisateur de votre serveur web (Apache, nginx, etc.)
+set :permission_method, :chown # Dans le cas où vous n’avez pas les ACLs, ne pas oublier de mettre :use_sudo à true
+set :use_set_permissions, true
+set :dump_assetic_assets, true # dumper les assets
+
+#default_run_options[:pty] = true # Si vous avez cette erreur : no tty present and no askpass program specified, alors décommentez
+#ssh_options[:forward_agent] = true # Idem que ci-dessus
+
+# Permet d’avoir le détail des logs de capistrano, plus facile à débugger si vous rencontrer des erreurs
+logger.level = Logger::MAX_LEVEL
+
+# Et enfin, si jamais vous rencontrez des erreurs de permissions, vous pouvez rajouter ces lignes suivantes :
+after "deploy:finalize_update" do
+run "chown -R root:www-data #{latest_release}"
+run "sudo chmod -R 777 #{latest_release}/#{cache_path}"
+run "sudo chmod -R 777 #{latest_release}/#{log_path}"
+end
